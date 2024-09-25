712e9fc98d2022aed4ed73ccdda52bf1c3650a91
Edward Tanguay (windows) <edward@tanguay.info>
2024-09-25 19:53:49 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './style.css'
+
+const createTitle = (part1: string, part2: string) => {
+  return part1 + part2;
+}
+
+
+const title:string = "info site";
+const num:number = 34;
+
+// const title2 = title + num;
+const title2 = createTitle(title, num);
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1>${title}</h1>
+  <h2>${title2}</h2>
+  <p>The number is ${num}.</p>
+`
